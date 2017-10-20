e614fc5ffd11d3a3dc0a5e7d899c5a2fcacf1d4e
Scott Brown <mail@typicalrunt.me>
2017-10-20 08:06:50 -0700

    Adding bosky 0.0.1 release

---
+++ bosky.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Bosky < Formula
+  homepage 'https://github.com/scottbrown/bosky'
+  url 'https://github.com/scottbrown/bosky/releases/download/0.0.1/bosky.0.0.1.darwin-amd64.tgz'
+  version '0.0.1'
+  sha256 '9cc7be421c9380e3c536115f345adf3493d11826ed22d885010d491030e8108e'
+
+  depends_on :arch => :x86_64
+
+  def install
+    bin.install 'bosky'
+  end
+
+  test do
+    system "#{bin}/bosky"
+  end
+end
