669b9c4e1a5aa1e2be9fee8ae77d2679a45de90e
Scott Brown <mail@typicalrunt.me>
2017-10-20 08:53:17 -0700

    Updating bosky to 0.0.2 release

--- bosky.rb
+++ bosky.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class Bosky < Formula
   homepage 'https://github.com/scottbrown/bosky'
-  url 'https://github.com/scottbrown/bosky/releases/download/0.0.1/bosky.0.0.1.darwin-amd64.tgz'
-  version '0.0.1'
-  sha256 '9cc7be421c9380e3c536115f345adf3493d11826ed22d885010d491030e8108e'
+  url 'https://github.com/scottbrown/bosky/releases/download/0.0.2/bosky_0.0.2_darwin_amd64.tar.gz'
+  version '0.0.2'
+  sha256 '375bca3bdcdba00dafc903c2b66f07f729cf54cd26f1d5230b11f58ae4473801'
 
   depends_on :arch => :x86_64
 
   def install
-    bin.install 'bosky.darwin-amd64'
+    bin.install 'bosky'
   end
 
   test do
-    system "#{bin}/bosky.darwin-amd64"
+    system "#{bin}/bosky"
   end
 end
